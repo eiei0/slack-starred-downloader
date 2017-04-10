cbf8fe774ed4a2c7cf3ea87c5763710cf10086ee
Jonathan MacDonald <j0nnyappleseed000@mac.com>
2017-04-10 09:34:19 -0500

    Organize methods
    Relocated methods from script into a class to organize code a little
    better.

---
+++ lib/slack_json_parser.rb
@@ -0,0 +1,73 @@
+require 'json'
+require 'open-uri'
+require 'httparty'
+
+class SlackJsonParser
+
+  def initialize
+  end
+
+  def generate_json(api_method, token)
+    path = "api/#{api_method}?token=#{token}&pretty=1"
+    response = HTTParty.get("https://slack.com/#{path}")
+
+    response['paging'] ? generate_json_pages(response, path) : response
+  end
+
+  def format_messages(starred_json, user_json)
+    message = ""
+    starred_json['items'].each do |item|
+      if item['message']
+        user_id = item['message']['user']
+
+        message.concat(
+          "## From: #{user_name(user_json, user_id)}\n" +
+          "#{item['message']['text']}\n\n"
+        )
+      elsif item['file']
+        user_id = item['file']['user']
+
+        message.concat(
+          "## From: #{user_name(user_json, user_id)}\n" +
+          "#{item['file']['name']}\n" +
+          "#{item['file']['text']}\n" +
+          "#{item['file']['url_private_download']}\n\n"
+        )
+      else
+        print "\nStarred message type currently not supported" && next
+      end
+    end
+    message
+  end
+
+  def team_name(team_response=nil)
+    if team_response.nil?
+      team_response = generate_json("team.info", SLACK_API_TOKEN)
+    end
+    team_response.parsed_response["team"]["name"]
+  end
+
+  def user_name(user_json, user_id)
+    users = user_json['members']
+    users.each do |user|
+      if user['id'] == user_id
+        return user['real_name']
+      end
+    end
+  end
+
+  private
+
+  def generate_json_pages(response, path)
+    last_page = response['paging']['pages']
+
+    if last_page > 1
+      (1..last_page).to_a.each do |page_number|
+        break HTTParty.get("https://slack.com/#{path}/&page=#{page_number}")
+      end
+    else
+      response
+    end
+  end
+
+end
--- run.rb
+++ run.rb
@@ -1,88 +1,25 @@
-require 'json'
-require 'open-uri'
-require 'httparty'
 require 'highline/import'
 require 'gist'
-require 'pry'
+require_relative 'lib/slack_json_parser.rb'
 
 system "clear"
-SLACK_API_TOKEN = ask "Find your Slack API token here: https://api.slack.com/custom-integrations/legacy-tokens\nEnter your Slack API token:\n"
+SLACK_API_TOKEN = ask 'Find your Slack API token here: '\
+  "https://api.slack.com/custom-integrations/legacy-tokens \nEnter your Slack API token:\n"
 system "clear"
 
 Gist.login!
 system "clear"
 
-def generate_json_pages(response, path)
-  last_page = response['paging']['pages']
+parser = SlackJsonParser.new
+user_json = parser.generate_json("users.list", SLACK_API_TOKEN)
+starred_json = parser.generate_json("stars.list", SLACK_API_TOKEN)
 
-  if last_page > 1
-    (1..last_page).to_a.each do |page_number|
-      break HTTParty.get("https://slack.com/#{path}/&page=#{page_number}")
-    end
-  else
-    response
-  end
-end
-
-def generate_json(api_method, token)
-  path = "api/#{api_method}?token=#{token}&pretty=1"
-  response = HTTParty.get("https://slack.com/#{path}")
-
-  response['paging'] ? generate_json_pages(response, path) : response
-end
-
-user_json = generate_json("users.list", SLACK_API_TOKEN)
-starred_json = generate_json("stars.list", SLACK_API_TOKEN)
-
-def team_name(team_response=nil)
-  if team_response.nil?
-    team_response = generate_json("team.info", SLACK_API_TOKEN)
-  end
-  team_response.parsed_response["team"]["name"]
-end
-
-def user_name(user_json, user_id)
-  users = user_json['members']
-  users.each do |user|
-    if user['id'] == user_id
-      return user['real_name']
-    end
-  end
-end
-
-
-SELECTED = ask "Continue exporting #{team_name} starred messages [yes/no]\n"
+selected = ask "Continue exporting #{parser.team_name} starred messages [yes/no]\n"
 system "clear"
-SELECTED == 'yes' ? print("exporting...\n") : print("aborting task") && return
+selected == 'yes' ? print("exporting...\n") : print("aborting task") && return
 
-def format_messages(starred_json, user_json)
-  message = ""
-  starred_json['items'].each do |item|
-    if item['message']
-      user_id = item['message']['user']
-
-      message.concat(
-        "## From: #{user_name(user_json, user_id)}\n" +
-        "#{item['message']['text']}\n\n"
-      )
-    elsif item['file']
-      user_id = item['file']['user']
-
-      message.concat(
-        "## From: #{user_name(user_json, user_id)}\n" +
-        "#{item['file']['name']}\n" +
-        "#{item['file']['text']}\n" +
-        "#{item['file']['url_private_download']}\n\n"
-      )
-    else
-      print "\nStarred message type currently not supported" && next
-    end
-  end
-  message
-end
-
-file = File.new("starred_messages_from_#{team_name}.md", "w")
-file.puts("#{format_messages(starred_json, user_json)}")
+file = File.new("starred_messages_from_#{parser.team_name}.md", "w")
+file.puts("#{parser.format_messages(starred_json, user_json)}")
 file.close
 
 Gist.gist("#{File.read(file)}", filename: "#{file.path}")
